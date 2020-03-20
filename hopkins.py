65ff6aa071f1529f19d6bd9cba1e8a07cce3b0fc
DavidGrogan <david@33n.co.uk>
2020-03-20 14:09:23 +0000

    Data processing for John Hopkins data

--- hopkins.py
+++ hopkins.py
@@ -14,22 +14,28 @@ recovered = pd.read_csv(
 frames = {"Confirmed": confirmed, "Deaths": deaths, "Recovered": recovered}
 
 for key, frame in frames.items():
-    frames[key] = frames[key].drop(columns=["Province/State", "Lat", "Long"], axis=1)
-    frames[key] = frames[key].groupby("Country/Region").sum().reset_index()
-    frames[key] = frames[key].melt(id_vars="Country/Region", var_name="Date")
-    frames[key] = frames[key].rename(columns={"value": key})
+    frames[key] = (
+        frames[key]
+        .drop(columns=["Province/State", "Lat", "Long"], axis=1)
+        .groupby("Country/Region")
+        .sum()
+        .reset_index()
+    )
+    frames[key] = (
+        frames[key]
+        .melt(id_vars="Country/Region", var_name="Date")
+        .rename(columns={"value": key})
+    )
     frames[key]["Date"] = pd.to_datetime(frames[key]["Date"])
     frames[key] = frames[key].set_index("Date")
 
 for key, frame in frames.items():
-    date_group = frames[key].groupby("Date")[key].sum()
-    date_group.plot()
+    frames[key].groupby("Date")[key].sum().plot()
     plt.legend()
 plt.show()
 
 for key, frame in frames.items():
     country_frame = frames[key][frames[key]["Country/Region"] == "United Kingdom"]
-    date_group = country_frame.groupby("Date")[key].sum()
-    date_group.plot()
+    country_frame.groupby("Date")[key].sum().plot()
     plt.legend()
 plt.show()
